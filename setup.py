07d8892856e35db308a31438fe1a586efa6fa486
leviathan <leviathan1995@outlook.com>
2015-08-09 22:33:44 +0800

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='pylsy',
+    packages=['pylsy'],
+    version='0.1',
+    description='A simple Python library for easily displaying tabular data ina visually appealing ASCII table format',
+    author='leviathan',
+    author_email='leviathan1995@outlook.com',
+    url='https://github.com/Leviathan1995/Pylsy',
+)
