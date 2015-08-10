dfa4d59cbec6b6e509ba711bed4dda8a9cfe11f9
leviathan <leviathan1995@outlook.com>
2015-08-10 12:39:49 +0800

    update

--- tests/test_pylsy.py
+++ tests/test_pylsy.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'leviathan'
-from  pylsy.pylsy import PylsyTable
+from  pylsy import PylsyTable
 def main():
-    attributes=["name","age","sex"]
+    attributes=["name","age","sex","id","time"]
     table=PylsyTable(attributes)
-    name=["sun","lsy"]
+    name=["sun","lsy","luna","leviathan"]
     table.AddData("name",name)
-    age=[20,21]
+    age=[20,21,23,25]
     table.AddData("age",age)
+    sex=["M","F","F","F"]
+    table.AddData("sex",sex)
+    id=[2001,2005,4242,02]
+    table.AddData("id",id)
+    time=["2015-8-10 11:29:54","2014-06-08 13:25:07","2014-06-0813:25:07","2014-06-08 13:25:07"]
+    table.AddData("time",time)
     table.CreateTable()
 
 if __name__ == '__main__':
