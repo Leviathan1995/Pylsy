6c288a5f7f15e95b38ddeb4a7ff39eaf80b169b1
Sinux <wshmelo@gmail.com>
2015-08-14 15:41:46 +0800

    Fix syntax to follow the PEP8, include naming rule, indentation, usage of blank and some other normal rules.

--- pylsy/pylsy.py
+++ pylsy/pylsy.py
@@ -1,80 +1,86 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-class PylsyTable:
-    def __init__(self,attributes):
-        self.Attributes=attributes
-        self.Table=[]
-        self.AttributesLength=[]
-        self.Colsnum=len(self.Attributes)
-        self.Linesnum=0
+
+class PylsyTable(object):
+    def __init__(self, attributes):
+        self.Attributes = attributes
+        self.Table = []
+        self.AttributesLength = []
+        self.cols_num = len(self.Attributes)
+        self.lines_num = 0
         for attribute in self.Attributes:
-            col=dict()
-            col[attribute]=""
+            col = dict()
+            col[attribute] = ""
             self.Table.append(col)
-    def PrintDivide(self):
+
+    def print_divide(self):
         for space in self.AttributesLength:
             print("+ ", end='')
             for sign in range(space):
                 print("- ", end='')
         print("+")
-    def AddData(self,attribute,values):
+
+    def add_data(self, attribute, values):
         for col in self.Table:
             if attribute in col:
-                dictvalues=[]
+                dict_values = []
                 for value in values:
-                    if type(value)!=str:
-                        dictvalues.append(str(value))
+                    if type(value) != str:
+                        dict_values.append(str(value))
                     else:
-                        dictvalues.append(value)
-                col[attribute]=dictvalues
-    def CreateTable(self):
+                        dict_values.append(value)
+                col[attribute] = dict_values
+
+    def create_table(self):
         for col in self.Table:
-            values=list(col.values())[0]
-            if self.Linesnum<len(values):
-                self.Linesnum=len(values)
+            values = list(col.values())[0]
+            if self.lines_num < len(values):
+                self.lines_num = len(values)
             # find the length of longest word in current column
-            Len=len(list(col.keys())[0])
+            key_length = len(list(col.keys())[0])
             for value in values:
-                length=len(value)
-                if length>Len:
-                    Len=length
-            self.AttributesLength.append(Len)
-        self.PrintHead()
-        self.PrintValue()
-    def PrintHead(self):
-        self.PrintDivide()
+                length = len(value)
+                if length > key_length:
+                    key_length = length
+            self.AttributesLength.append(key_length)
+        self.print_head()
+        self.print_value()
+
+    def print_head(self):
+        self.print_divide()
         print("| ", end='')
-        for spaces,attr in zip(self.AttributesLength,self.Attributes):
-            spacenum=spaces*2-1
-            start=(spacenum-len(attr))//2
+        for spaces, attr in zip(self.AttributesLength, self.Attributes):
+            space_num = spaces * 2 - 1
+            start = (space_num - len(attr)) // 2
             for space in range(start):
                 print(" ", end='')
             print(attr + ' ', end='')
-            end=spacenum-start-len(attr)
+            end = space_num - start - len(attr)
             for space in range(end):
                 print(" ", end='')
             print("| ", end='')
         print("")
-        self.PrintDivide()
-    def PrintValue(self):
-        for line in range(self.Linesnum):
-            for col,length in zip(self.Table,self.AttributesLength):
+        self.print_divide()
+
+    def print_value(self):
+        for line in range(self.lines_num):
+            for col, length in zip(self.Table, self.AttributesLength):
                 print("| ", end='')
-                valuelength=length*2-1
-                value=list(col.values())[0]
-                if len(value)!=0:
-                    start=(valuelength-len(value[line]))//2
+                value_length = length * 2 - 1
+                value = list(col.values())[0]
+                if len(value) != 0:
+                    start = (value_length - len(value[line])) // 2
                     for space in range(start):
                         print(" ", end='')
                     print(value[line] + ' ', end='')
-                    end=valuelength-start-len(value[line])
+                    end = value_length - start - len(value[line])
                     for space in range(end):
                         print(" ", end='')
                 else:
-                    start=0
-                    end=valuelength-start+1
-                    for sapce in range(end):
+                    start = 0
+                    end = value_length - start + 1
+                    for space in range(end):
                         print(" ", end='')
             print("|")
-            self.PrintDivide()
+            self.print_divide()
--- tests/test_pylsy.py
+++ tests/test_pylsy.py
@@ -5,16 +5,16 @@ def main():
     attributes=["name","age","sex","id","time"]
     table=PylsyTable(attributes)
     name=["sun","lsy","luna","leviathan"]
-    table.AddData("name",name)
+    table.add_data("name",name)
     age=[20,21,23,25]
-    table.AddData("age",age)
+    table.add_data("age",age)
     sex=["M","F","F","F"]
-    table.AddData("sex",sex)
+    table.add_data("sex",sex)
     id=[2001,2005,4242,02]
-    table.AddData("id",id)
+    table.add_data("id",id)
     time=["2015-8-10 11:29:54","2014-06-08 13:25:07","2014-06-0813:25:07","2014-06-08 13:25:07"]
-    table.AddData("time",time)
-    table.CreateTable()
+    table.add_data("time",time)
+    table.create_table()
 
 if __name__ == '__main__':
     main()
