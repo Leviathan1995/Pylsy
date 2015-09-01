048e9a60d9169f41888c95181e83da8eff985774
Leviathan <Leviathan1995@outlook.com>
2015-09-01 16:06:50 +0800

    Update pylsy.py

--- pylsy/pylsy.py
+++ pylsy/pylsy.py
@@ -2,8 +2,8 @@
 from __future__ import print_function
 
 class PylsyTable(object):
-    
-    
+       
+           
     def __init__(self, attributes):
         self.Attributes = attributes
         self.Table = []
